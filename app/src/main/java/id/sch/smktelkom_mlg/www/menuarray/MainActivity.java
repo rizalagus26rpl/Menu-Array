ebcfcf94d2f61ae163e90546fdfae9e68fac9b8c
Rizal Agus Atmaji <rizal_agus_26rpl@student.smktelkom-mlg.sch.id>
2019-02-27 19:01:55 +0700

    selesai

---
+++ app/src/main/java/id/sch/smktelkom_mlg/www/menuarray/MainActivity.java
@@ -0,0 +1,40 @@
+package id.sch.smktelkom_mlg.www.menuarray;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+Button btnSubmit;
+    EditText editNama, editTahun;
+    Spinner spKota, spProvinsi;
+    TextView txtHasil;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnSubmit = (Button)findViewById(R.id.btnSubmit);
+        editNama = (EditText)findViewById(R.id.editNama);
+        editTahun = (EditText)findViewById(R.id.editTahun);
+        spKota = (Spinner)findViewById(R.id.spKota);
+        spProvinsi = (Spinner)findViewById(R.id.spProvinsi);
+        txtHasil = (TextView)findViewById(R.id.txtHasil);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nama = editNama.getText().toString();
+                int tahun = Integer.parseInt(editTahun.getText().toString());
+                int usia = 2019 - tahun;
+                txtHasil.setText("Nama Anda "+nama+"\n"+"Anda berusia "+usia+"Tahun"+"\n"+
+                        "Anda berasal dari kota "+spKota.getSelectedItem().toString()+"\n"+
+                        "Provinsi "+spProvinsi.getSelectedItem().toString());
+            }
+        });
+    }
+}
